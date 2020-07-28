7a0d72c74552f20d92e799bd533ae702bbedfb3d
johan-avila <johan-avila@outlook.com>
2020-07-28 03:37:39 -0500

    a dormir

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,13 @@
-const video = document.getElementById("video")
+import MediaPlayer from "./MediPlayer.js"
+import AutoPlay from "./plugin/Autoplay.js"
+
+const vide = document.getElementById("video")
 const button = document.getElementById("playButton")
 
-function MediaPlayer(config) {
-    this.media = config.el
-
-}
-
-MediaPlayer.prototype.play = function () {
-    this.media.play()
-}
-MediaPlayer.prototype.pause = function () {
-    this.media.pause()
-}
-MediaPlayer.prototype.pausado = function () {
-    return this.media.paused;
-}
 
 const player = new MediaPlayer({
-    el: video
+    el: vide,
+    plugin: [new AutoPlay()]
 })
 
 button.onclick = () => {
---
+++ assets/plugin/Autoplay.js
@@ -0,0 +1,5 @@
+function Autoplay (){
+
+}
+
+export default Autoplay
